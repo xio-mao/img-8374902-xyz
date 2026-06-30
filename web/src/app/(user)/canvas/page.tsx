8fa978db830f219f7005c9a5ac95e822c48e8450
xio-mao <89598690+xio-mao@users.noreply.github.com>
2026-07-01 00:14:15 +0800

    Update page.tsx

--- web/src/app/(user)/canvas/page.tsx
+++ web/src/app/(user)/canvas/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useEffect, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
